7bf0053a87b2207082e33ab491ab24ec82380aa7
John <elsporko@gmail.com>
2019-02-10 17:30:42 -0500

    Put receive message behavior in a thread so that it is no longer blocked while waiting for text input.

--- client.py
+++ client.py
@@ -1,5 +1,25 @@
 from aws import AWS
 import json
+import threading
+
+def get_messages(aws):
+    # Retrieve and print and remove any pending messages
+    while True:
+        payload = aws.receive_message()
+        if payload:
+            try:
+                for msg in payload['Messages']:
+                    receipthandle=msg['ReceiptHandle']
+                    msg=json.loads(msg['Body'])
+                    if 'Message' in msg:
+                        msg=msg['Message']
+
+                        print ("\n\n+++++++++++++++++++++++++++++++++++++++++++++++")
+                        print (msg)
+                        print ("+++++++++++++++++++++++++++++++++++++++++++++++")
+                        aws.delete_message(receipthandle=receipthandle)
+            except KeyError:
+                pass
 
 aws = AWS()
 my_topic=input('Input topic name: ')
@@ -17,33 +37,20 @@ aws.add_policy()
 # Subscribe to my topic
 aws.subscribe_to_topic()
 
+t = threading.Thread(target = get_messages, args=(aws,))
+t.start()
+
+
 o_topic=input('Who would you like to speak with? ')
 rt = aws.register_target(o_topic)
 
 while True:
-    # Retrieve and print and remove any pending messages
-    payload = aws.receive_message()
-    if payload:
-        try:
-            for msg in payload['Messages']:
-                receipthandle=msg['ReceiptHandle']
-                msg=json.loads(msg['Body'])
-                if 'Message' in msg:
-                    msg=msg['Message']
-
-                    print ("+++++++++++++++++++++++++++++++++++++++++++++++")
-                    print (msg)
-                    print ("+++++++++++++++++++++++++++++++++++++++++++++++")
-                    aws.delete_message(receipthandle=receipthandle)
-        except KeyError:
-            pass
-    # Remove message from queue after procesing
-    
     try:
         msg = input('What would you like to say: ')
     except KeyboardInterrupt:
         aws.delete_topic()
         aws.delete_queue()
+        t.join()
         print("\nProcess complete")
         break
 
